df9f5f36d4f1e646fd1c36a2e3fcf3a2cc6dc62f
Ajay Thorve <athorve@nvidia.com>
2021-07-22 11:39:27 -0700

    update

--- Data Processing/data_gen.py
+++ Data Processing/data_gen.py
@@ -1,23 +1,20 @@
 import cudf
-import pandas as pd
-import cugraph
-import cuspatial
 import pathlib
 from pyrosm import get_data, OSM
 DATA_DIR = "../data/"
-from constants import list_of_states
+from constants import regions
 
 
-failed_states = []
-for state in list_of_states:
-    if not(pathlib.Path(f"{DATA_DIR}{state}-nodes.parquet").is_file() and pathlib.Path(f"{DATA_DIR}{state}-nodes.parquet").is_file()):
-        print("starting processing for state "+state)
+failed_regions = []
+for region in regions:
+    if not(pathlib.Path(f"{DATA_DIR}{region}-nodes.parquet").is_file() and pathlib.Path(f"{DATA_DIR}{region}-nodes.parquet").is_file()):
+        print("starting processing for region "+region)
         try:
-            fp = get_data(state, directory=DATA_DIR)
+            fp = get_data(region, directory=DATA_DIR)
             osm = OSM(fp)
         except:
-            print("failed for state", state)
-            failed_states.append(state)
+            print("failed for region", region)
+            failed_regions.append(region)
             continue
         print("loading network")
         nodes, edges = osm.get_network(nodes=True, network_type="driving")
@@ -27,15 +24,15 @@ for state in list_of_states:
         nodes_cudf = cudf.from_pandas(nodes[['lon', 'lat', 'id']])
         nodes_cudf.columns = ["x", "y", "vertex"]
         print("saving as parquet files")
-        nodes_cudf.to_parquet(DATA_DIR+f"{state}-nodes.parquet")
-        edges_cudf.to_parquet(DATA_DIR+f"{state}-edges.parquet")
-        print(f"processing for state {state} complete")
+        nodes_cudf.to_parquet(DATA_DIR+f"{region}-nodes.parquet")
+        edges_cudf.to_parquet(DATA_DIR+f"{region}-edges.parquet")
+        print(f"processing for region {region} complete")
         del(nodes_cudf, edges_cudf, nodes, edges, fp, osm)
     else:
-        print(f"state {state} already processed")
+        print(f"region {region} already processed")
 
 
-print("states failed", failed_states)
+print("regions failed", failed_regions)
 
 
 # bin/osmosis --read-pbf ../data/north-carolina-latest.osm.pbf --tee 4 --bounding-box left=-117 top=38 --write-pbf northcaliforniaSE.osm.pbf --bounding-box left=-117 bottom=38 --write-pbf northcaliforniaNE.osm.pbf --bounding-box right=-117 top=38 --write-pbf northcaliforniaSW.osm.pbf --bounding-box right=-117 bottom=38 --write-pbf northcaliforniaNW.osm.pbf
